b4be63e85b9823e5e1a78fdfd8cb1bb5298b5b87
gasToledo <mgaston648@gmail.com>
2023-04-22 19:38:42 -0300

    Cambios en todo el proyecto, se dejo de utilizar todo lo predeterminado, algunas configuraciones en el manifest y el comienzo de la pantalla de MAPA con sus toolbar y bottombars

---
+++ app/src/main/java/ar/edu/unlam/mobile2/pantallaMapa/BottomNavItem.kt
@@ -0,0 +1,9 @@
+package com.example.pantallastp.pantallaMapa
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavItem(
+    val name: String,
+    val route: String,
+    val icon: ImageVector
+)
